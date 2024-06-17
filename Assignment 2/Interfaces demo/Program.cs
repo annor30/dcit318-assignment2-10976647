49dce3adf4c798cda0a637132879afdbf297213b
annor30 <103123219+annor30@users.noreply.github.com>
2024-06-17 16:04:52 +0000

    Add files via upload

---
+++ Assignment 2/Interfaces demo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public interface IMovable
+{
+    void Move();
+}
+
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+
+        car.Move();
+        bicycle.Move();
+    }
+}
---
+++ Assignment 2/abstract demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+public class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Circle circle = new Circle { Radius = 5 };
+        Rectangle rectangle = new Rectangle { Width = 5, Height = 8 };
+
+        Console.WriteLine($"Circle area: {circle.GetArea()}");
+        Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+    }
+}
